c9add8fb2cc65750fc64cdb758537b6163dbd4e3
Jiawen He <hanghur@gmail.com>
2024-01-12 16:07:56 +1100

    test: linear regression and log_prior tests

---
+++ examples/linear_regression.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import bayesbridge as bb
+
+
+# constants
+M_TRUE = np.array([20, -10, -3, 1])
+N_DIMS = len(M_TRUE)
+DATA_NOISE_STD = 20
+
+# generate synthetic data
+x = np.arange(-5, 10)
+fwd_jacobian = np.vander(x, N_DIMS, True)
+y = fwd_jacobian @ M_TRUE
+y_noisy = y + np.random.normal(0, DATA_NOISE_STD, y.shape)
+
+# define parameters
+coefficients_param = bb.parameters.UniformParameter("coefficients", -50, 50, 3)
+
+# define parameterization
+param_space = bb.parameterization.ParameterSpace(
+    name="my_param_space", 
+    n_dimensions=4, 
+    parameters=[coefficients_param], 
+)
+parameterization = bb.parameterization.Parameterization(param_space)
+
+# define forward function
+def my_fwd(state: bb.State) -> np.ndarray:
+    m = state["my_param_space"]["coefficients"]
+    return fwd_jacobian @ m
+fwd_functions = [my_fwd]
+
+# define data target
+targets = [bb.Target("my_data", y, 1/DATA_NOISE_STD**2)]
+
+# run the sampling
+inversion = bb.BayesianInversion(
+    parameterization=parameterization, 
+    targets=targets, 
+    fwd_functions=fwd_functions, 
+    n_chains=3, 
+    n_cpus=3, 
+)
+inversion.run(
+    sampler=None, 
+    n_iterations=500_000, 
+    burnin_iterations=4_000, 
+    save_every=1000, 
+    print_every=1000, 
+)
+
+# get results and plot
+results = inversion.get_results()
+coefficients_samples = results["coefficients"]
+fig, ax = plt.subplots()
+for i, coefficients in enumerate(coefficients_samples):
+    y_pred = fwd_jacobian @ coefficients
+    if i == 0:
+        ax.plot(x, y_pred, c="gray", lw=0.05, label="Predicted data from samples")
+    else:
+        ax.plot(x, y_pred, c="gray", lw=0.05)
+ax.plot(x, y, c="orange", label="Noise-free data from true model")
+ax.scatter(x, y_noisy, c="purple", label="Noisy data used for inference", zorder=3)
+ax.legend()
+fig.savefig("linear_regression_samples_pred")
---
+++ tests/test_param_space_log_prior.py
@@ -0,0 +1,33 @@
+import math
+import random
+import numpy as np
+import bayesbridge as bb
+
+
+uniform_param = bb.parameters.UniformParameter("uniform_param", -1, 1, 0.1)
+gaussian_param = bb.parameters.GaussianParameter("gaussian_param", 0, 1, 0.1)
+custom_param = bb.parameters.CustomParameter(
+    name="custom_param",
+    log_prior=lambda v: 0 if 0 <= v <= 1 else float("-inf"), 
+    initialize=(lambda p: \
+        np.random.uniform(0,1,len(p)) \
+            if (not np.isscalar(p) and p is not None) \
+                else random.uniform(0,1)), 
+    perturb_std=1, 
+)
+
+my_ps = bb.parameterization.ParameterSpace(
+    name="test", 
+    n_dimensions=1, 
+    parameters=[uniform_param, gaussian_param, custom_param], 
+)
+
+my_test_pss = bb.ParameterSpaceState(1, {
+    "uniform_param": np.array([0,]), 
+    "gaussian_param": np.array([0,]), 
+    "custom_param": np.array([0.5,]), 
+})
+
+result = my_ps.log_prior(my_test_pss)
+expected = math.log(1/2) + math.log(1/(math.sqrt(2*math.pi)*math.exp(0))) + 0
+assert result == expected
