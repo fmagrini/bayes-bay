4dfff8266a59aa755403435f87aca57620fc6e42
Fabrizio Magrini <98057074+fmagrini@users.noreply.github.com>
2024-01-12 18:13:44 +1100

    docs final!

--- src/bayesbridge/discretization/_discretization.py
+++ src/bayesbridge/discretization/_discretization.py
@@ -10,12 +10,48 @@ from ..perturbations._param_values import ParamPerturbation
 
 
 class Discretization(Parameter, ParameterSpace):
-    
+    r"""Low-level class to define a discretization
+
+    Parameters
+    ----------
+    name : str
+        name of the discretization, for display and storing purposes
+    spatial_dimensions : int
+        number of spatial dimensions of the discretization, e.g. 1D, 2D, or 3D.
+    vmin, vmax : Union[Number, np.ndarray]
+        minimum/maximum value bounding each dimension
+    perturb_std : Union[Number, np.ndarray]
+        standard deviation of the Gaussians used to randomly perturb the discretization
+        in each dimension. 
+    n_dimensions : Number, optional
+        number of dimensions. None (default) results in a trans-dimensional
+        discretization, with the dimensionality of the parameter space allowed
+        to vary in the range ``n_dimensions_min``-``n_dimensions_max``
+    n_dimensions_min, n_dimensions_max : Number, optional
+        minimum and maximum number of dimensions, by default 1 and 10. These
+        parameters are ignored if ``n_dimensions`` is not None, i.e. if the
+        discretization is not trans-dimensional
+    n_dimensions_init_range : Number, optional
+        percentage of the range `n_dimensions_min``-``n_dimensions_max`` used to
+        initialize the number of dimensions (0.3. by default). For example, if 
+        ``n_dimensions_min``=1, ``n_dimensions_max``=10, and ``n_dimensions_init_range``=0.5,
+        the maximum number of dimensions at the initialization is
+        
+        .. code-block:: python
+            
+            int((n_dimensions_max - n_dimensions_min) * n_dimensions_init_range + n_dimensions_max)
+            
+    parameters : List[Parameter], optional
+        a list of free parameters, by default None
+    birth_from : {"prior", "neighbour"}, optional
+        whether to initialize the newborn basis functions by randomly drawing from 
+        the prior (default) or by perturbing the neighbor one.
+    """
     def __init__(
         self,
         name: str,
         spatial_dimensions: Number,
-        perturb_std: Union[Number, np.ndarray],
+        perturb_std: Union[Number, np.ndarray] = 0.1,
         n_dimensions: int = None, 
         n_dimensions_min: int = 1, 
         n_dimensions_max: int = 10, 
@@ -63,18 +99,102 @@ class Discretization(Parameter, ParameterSpace):
     def birth(
         self, param_space_state: ParameterSpaceState
     ) -> Tuple[ParameterSpaceState, Number]:
+        r"""adds a dimension to the current parameter space and returns the
+        thus obtained new state along with the log of the corresponding partial 
+        acceptance probability,
+        
+        .. math::
+            \underbrace{\alpha_{p}}_{\begin{array}{c} \text{Partial} \\ \text{acceptance} \\ \text{probability} \end{array}} = 
+            \underbrace{\frac{p\left({\bf m'}\right)}{p\left({\bf m}\right)}}_{\text{Prior ratio}} 
+            \underbrace{\frac{q\left({\bf m} \mid {\bf m'}\right)}{q\left({\bf m'} \mid {\bf m}\right)}}_{\text{Proposal ratio}}  
+            \underbrace{\lvert \mathbf{J} \rvert}_{\begin{array}{c} \text{Jacobian} \\ \text{determinant} \end{array}}.
+            
+        Parameters
+        ----------
+        ParameterSpaceState
+            initial parameter space state
+
+        Returns
+        -------
+        ParameterSpaceState
+            new parameter space state
+        Number
+            log of the partial acceptance probability, 
+            :math:`\alpha_{p} = \log(
+            \frac{p({\bf m'})}{p({\bf m})}
+            \frac{q\left({\bf m} 
+            \mid {\bf m'}\right)}{q\left({\bf m'} \mid {\bf m}\right)}
+            \lvert \mathbf{J} \rvert)`
+        """
         raise NotImplementedError
     
     @abstractmethod
     def death(
         self, param_space_state: ParameterSpaceState
     ) -> Tuple[ParameterSpaceState, Number]:
+        r"""removes a dimension from the given parameter space and returns the
+        thus obtained new state along with the log of the corresponding partial 
+        acceptance probability,
+        
+        .. math::
+            \underbrace{\alpha_{p}}_{\begin{array}{c} \text{Partial} \\ \text{acceptance} \\ \text{probability} \end{array}} = 
+            \underbrace{\frac{p\left({\bf m'}\right)}{p\left({\bf m}\right)}}_{\text{Prior ratio}} 
+            \underbrace{\frac{q\left({\bf m} \mid {\bf m'}\right)}{q\left({\bf m'} \mid {\bf m}\right)}}_{\text{Proposal ratio}}  
+            \underbrace{\lvert \mathbf{J} \rvert}_{\begin{array}{c} \text{Jacobian} \\ \text{determinant} \end{array}}.
+                    
+        Parameters
+        ----------
+        ParameterSpaceState
+            initial parameter space state
+
+        Returns
+        -------
+        ParameterSpaceState
+            new parameter space state
+        Number
+            log of the partial acceptance probability, 
+            :math:`\alpha_{p} = \log(
+            \frac{p({\bf m'})}{p({\bf m})}
+            \frac{q\left({\bf m} 
+            \mid {\bf m'}\right)}{q\left({\bf m'} \mid {\bf m}\right)}
+            \lvert \mathbf{J} \rvert)`
+        """
         raise NotImplementedError
 
     @abstractmethod
     def perturb_value(
-        self, param_space_state: ParameterSpaceState, isite: int
+        self, param_space_state: ParameterSpaceState, idimension: int
     ) -> Tuple[ParameterSpaceState, Number]:
+        r"""perturbs the parameter space inherent to the discretization and 
+        calculates the log of the corresponding partial acceptance probability 
+        
+        .. math::
+            \underbrace{\alpha_{p}}_{\begin{array}{c} \text{Partial} \\ \text{acceptance} \\ \text{probability} \end{array}} = 
+            \underbrace{\frac{p\left({\bf m'}\right)}{p\left({\bf m}\right)}}_{\text{Prior ratio}} 
+            \underbrace{\frac{q\left({\bf m} \mid {\bf m'}\right)}{q\left({\bf m'} \mid {\bf m}\right)}}_{\text{Proposal ratio}}  
+            \underbrace{\lvert \mathbf{J} \rvert}_{\begin{array}{c} \text{Jacobian} \\ \text{determinant} \end{array}}.
+            
+        .. note::
+            All free parameters linked to the discretization will be perturbed
+            at the specified index.
+
+        Parameters
+        ----------
+        ParameterSpaceState
+            initial parameter space state
+        idimension : int
+            index of the parameter-space dimension to be perturbed
+
+        Returns
+        -------
+        Tuple[ParameterSpaceState, Number]
+            the perturbed value and
+            :math:`\alpha_{p} = \log(
+            \frac{p({\bf m'})}{p({\bf m})}
+            \frac{q\left({\bf m} 
+            \mid {\bf m'}\right)}{q\left({\bf m'} \mid {\bf m}\right)}
+            \lvert \mathbf{J} \rvert)`
+        """
         raise NotImplementedError
     
     @abstractmethod
@@ -82,6 +202,9 @@ class Discretization(Parameter, ParameterSpace):
         raise NotImplementedError
 
     def get_perturb_std(self, *args) -> Number:
+        """get the standard deviation of the Gaussian used to perturb the 
+        discretization
+        """
         return self.perturb_std
 
     def _repr_dict(self) -> dict:      # to be called by ParameterSpace.__repr__
