7ff43854ef4da873f67791a1361202d9067c5218
Jiawen He <hanghur@gmail.com>
2024-01-18 11:00:40 +1100

    docs: fix error in quickstart notebook

--- examples/linear_regression_hier.py
+++ examples/linear_regression_hier.py
@@ -3,16 +3,27 @@ import matplotlib.pyplot as plt
 import bayesbay as bb
 
 
-# constants
-M_TRUE = np.array([20, -10, -3, 1])
-N_DIMS = len(M_TRUE)
+# DIMENSIONS AND TRUE COEFFICIENTS
+N_DIMS = 4
+M0, M1, M2, M3 = 20, -10, -3, 1
+
+# DATA AND NOISE
+N_DATA = 15
+DATA_X = np.linspace(-5, 10, N_DATA)
 DATA_NOISE_STD = 20
 
 # generate synthetic data
-x = np.arange(-5, 10)
-fwd_jacobian = np.vander(x, N_DIMS, True)
-y = fwd_jacobian @ M_TRUE
+fwd_operator = np.vander(DATA_X, N_DIMS, True)
+y = fwd_operator @ [M0, M1, M2, M3]
 y_noisy = y + np.random.normal(0, DATA_NOISE_STD, y.shape)
+
+fig, ax = plt.subplots(dpi=200)
+ax.set_title('Synthetic data')
+ax.plot(DATA_X, y, 'k', label='Predicted data (true model)')
+ax.plot(DATA_X, y_noisy, 'ro', label='Noisy observations')
+ax.grid()
+ax.legend()
+plt.show()
 
 # define parameters
 m0 = bb.parameters.UniformParameter("m0", -100, 100, 5)
@@ -31,7 +42,7 @@ parameterization = bb.parameterization.Parameterization(param_space)
 # define forward function
 def my_fwd(state: bb.State) -> np.ndarray:
     m = [state["my_param_space"][f"m{i}"] for i in range(N_DIMS)]
-    return np.squeeze(fwd_jacobian @ np.array(m))
+    return np.squeeze(fwd_operator @ m)
 fwd_functions = [my_fwd]
 
 # define data target
@@ -59,15 +70,15 @@ coefficients_samples = np.squeeze(np.array([results[f"m{i}"] for i in range(N_DI
 fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 all_y_pred = np.zeros((coefficients_samples.shape[1], len(y)))
 for i, coefficients in enumerate(coefficients_samples.T):
-    y_pred = fwd_jacobian @ coefficients
+    y_pred = fwd_operator @ coefficients
     all_y_pred[i,:] = y_pred
     if i == 0:
-        axes[0].plot(x, y_pred, c="gray", lw=0.05, label="Predicted data from samples")
+        axes[0].plot(DATA_X, y_pred, c="gray", lw=0.05, label="Predicted data from samples")
     else:
-        axes[0].plot(x, y_pred, c="gray", lw=0.05)
-axes[0].plot(x, np.median(all_y_pred, axis=0), c="blue", label="Median predicted sample")
-axes[0].plot(x, y, c="orange", label="Noise-free data from true model")
-axes[0].scatter(x, y_noisy, c="purple", label="Noisy data used for inference", zorder=3)
+        axes[0].plot(DATA_X, y_pred, c="gray", lw=0.05)
+axes[0].plot(DATA_X, np.median(all_y_pred, axis=0), c="blue", label="Median predicted sample")
+axes[0].plot(DATA_X, y, c="orange", label="Noise-free data from true model")
+axes[0].scatter(DATA_X, y_noisy, c="purple", label="Noisy data used for inference", zorder=3)
 axes[0].legend()
 axes[1].hist(results["my_data.std"], bins=100, ec="w")
 fig.savefig("linear_regression_hier_samples_pred")
