9ceff312a7194559f482272e6f2620c734aefd8d
Fabrizio Magrini <98057074+fmagrini@users.noreply.github.com>
2024-02-29 17:43:58 +1100

    Update partition_modeling.py

--- examples/partition_modeling.py
+++ examples/partition_modeling.py
@@ -12,9 +12,12 @@ import matplotlib.pyplot as plt
 from bayesbay.discretization import Voronoi1D
 from bayesbay.prior import UniformPrior
 from bayesbay.parameterization import Parameterization
+from bayesbay import Target, LogLikelihood, BayesianInversion
+np.random.seed(30)
 
-X_DATA = np.linspace(0, 10, 50)
-NOISE_STD = 2
+
+X_DATA = np.linspace(0, 10, 200)
+NOISE_STD = 5
 
 
 @np.vectorize
@@ -63,12 +66,11 @@ def fwd_function(state):
 
 d_obs = piecewise_function(X_DATA) + np.random.normal(0, NOISE_STD, X_DATA.size)
 
-plt.step(X_DATA, piecewise_function(X_DATA), 'gray', lw=3, label='True model')
-plt.plot(X_DATA, d_obs, 'ro', markeredgecolor='k', label='Obs. data')
-plt.plot(X_DATA, d_pred, 'bo', markeredgecolor='k', label='Pred. data')
-plt.legend()
-plt.grid()
-
+# plt.step(X_DATA, piecewise_function(X_DATA), 'gray', lw=3, label='True model')
+# plt.plot(X_DATA, d_obs, 'ro', markeredgecolor='k', label='Obs. data')
+# plt.legend()
+# plt.grid()
+# plt.show()
 
 
 y = UniformPrior('y', vmin=-35, vmax=35, perturb_std=3.5)
@@ -79,14 +81,69 @@ voronoi = Voronoi1D(
     perturb_std=0.75,
     n_dimensions=None, 
     n_dimensions_min=2,
-    n_dimensions_max=30,
+    n_dimensions_max=40,
     parameters=[y], 
     birth_from='prior'
 )
 parameterization = Parameterization(voronoi)
 
 
+target = Target("d_obs", 
+                d_obs, 
+                std_min=0, 
+                std_max=20, 
+                std_perturb_std=2)
 
+log_likelihood = LogLikelihood(targets=target, fwd_functions=fwd_function)
+
+inversion = BayesianInversion(
+    parameterization=parameterization, 
+    log_likelihood=log_likelihood,
+    n_chains=10
+)
+#%%
+inversion.run(
+    sampler=None, 
+    n_iterations=300_000, 
+    burnin_iterations=75_000, 
+    save_every=200,
+    print_every=10_000,
+    verbose=False
+)
+
+
+#%%
+
+results = inversion.get_results(concatenate_chains=True)
+sampled_nuclei = results['voronoi.discretization']
+sampled_extents = [Voronoi1D.compute_cell_extents(n) for n in sampled_nuclei]
+sampled_y = results['voronoi.y']
+statistics = Voronoi1D.get_depth_profiles_statistics(
+    sampled_extents, sampled_y, X_DATA, percentiles=(10, 90)
+    )
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
+ax1.step(X_DATA, piecewise_function(X_DATA), 'gray', lw=3, label='True model')
+ax1.plot(X_DATA, statistics['median'], 'b', label='Inferred (median) model')
+ax1.fill_between(X_DATA, 
+                *statistics['percentiles'], 
+                color='b', 
+                alpha=0.2, 
+                label='Uncertainty (10th-90th percentiles)')
+ax1.plot(X_DATA, d_obs, 'ro', markeredgecolor='k', label='Obs. data')
+ax1.legend(framealpha=0)
+ax1.grid()
+
+pdf, bins, _ = ax2.hist(results['d_obs.std'], density=True, bins=20, ec='w', label='Posterior')
+ax2.axvline(x=NOISE_STD, color='r', lw=3, alpha=1, label='True data noise', zorder=100)
+ax2.fill_between([target.std_min, target.std_max], 
+                 1 / (target.std_max - target.std_min), 
+                 alpha=0.2, 
+                 label='Prior')
+ax2.set_xlabel('Noise standard deviation')
+ax2.legend(framealpha=0.9)
+plt.tight_layout()
+plt.show()
 
 
 
